6361cc73f6365a09c9473573a92874bc90acdcbd
mukankunziza opportunee <nshutioppo@yahoo.fr>
2018-12-20 18:36:08 +0200

    Add variable for questions

---
+++ scripts.js
@@ -0,0 +1,12 @@
+function check() {
+    var questionOne = document.Quiz.questionOne.value;
+    var questionTwo = document.Quiz.questionTwo.value;
+    var questionThree = document.Quiz.questionThree.value;
+    var questionFour = document.Quiz.questionFour.value;
+    var questionFive = document.Quiz.questionFive.value;
+    var questionSix = document.Quiz.questionSix.value;
+    var questionSeven = document.Quiz.questionSeven.value;
+    var questionEight = document.Quiz.questionEight.value;
+    var questionNine = document.Quiz.questionNine.value;
+    var questionTen = document.Quiz.questionTen.value;
+    var right = 0;
